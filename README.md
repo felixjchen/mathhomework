7b9c5f886188b020c464b5ffdeb010af45801593
felix <felixchen1998@gmail.com>
2022-09-01 14:45:20 -0400

    YES

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ https://gist.github.com/harlow/dbcd639cf8d396a2ab73#file-worker_refactored-go
 https://defiprime.com/polygon#decentralized-exchanges-on-polygon
 
 https://medium.com/rungo/everything-you-need-to-know-about-packages-in-go-b8bac62b74cc
+
+https://github.com/QuickSwap/quickswap-core/tree/master/contracts
--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 
-	"arbitrage_go/mypackage"
+	"arbitrage_go/constants"
 
 	"github.com/chenzhijie/go-web3"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 func main() {
-
-	mypackage.PrintHello()
-
 	// change to your rpc provider
-	var rpcProvider = "https://polygon-rpc.com"
-	web3, err := web3.NewWeb3(rpcProvider)
+	MUMBAI_URL := "https://polygon-mumbai.infura.io/v3/1de294ccc0da4f2ab105c9770ab3b962"
+	// POLYGON_URL := "https://polygon-rpc.com"
+	web3, err := web3.NewWeb3(MUMBAI_URL)
 	if err != nil {
 		panic(err)
 	}
@@ -23,4 +23,16 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Current block number: ", blockNumber)
+
+	contractAddr := "0x3060307bada5370d2dbf2f52640cc91975394de3" // contract address
+	contract, err := web3.Eth.NewContract(constants.UniswapFlashQueryABI, contractAddr)
+	fmt.Println("Contract address: ", contract.Address())
+
+	res, err := contract.Call("getPairsByIndexRange", common.HexToAddress("0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32"), big.NewInt(0), big.NewInt(10))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%x\n", res)
+	fmt.Printf("res %v\n", res)
 }
